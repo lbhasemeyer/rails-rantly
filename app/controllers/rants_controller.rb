7bd793ea51948d28a694bc7a0dce88e25efce6eb
lkbhas <lbhasemeyer@gmail.com>
2015-02-24 11:20:11 -0700

    set up rails backend with rants

--- app/controllers/rants_controller.rb
+++ app/controllers/rants_controller.rb
@@ -5,4 +5,40 @@ class RantsController < ApplicationController
     render json: @rants
   end
 
+  def show
+    @rant = Rant.find(params[:id])
+    render json: @rant
+  end
+
+  def create
+    @rant = Rant.new(rant_params)
+    if @rant.save
+      render json: @rant, status: :created, location: @rant
+    else
+      render json: @rant.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @rant = Rant.find(params[:id])
+    if @rant.update(post_params)
+      head :no_content
+    else
+      render json: @rant.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @rant = Rant.find(params[:id])
+    @rant.destroy
+    head :no_content
+  end
+
+
+  private
+
+  def rant_params
+    params.require(:rant).permit(:title, :body)
+  end
+
 end
