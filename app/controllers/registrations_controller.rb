2dbb561d160f93abd9ccc5f2d2037b2e42f9775b
lkbhas <lbhasemeyer@gmail.com>
2015-03-18 09:32:36 -0600

    adjusted params password

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -20,7 +20,7 @@ class RegistrationsController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :password_digest)
   end
 
 end
